c587b01e8f2184eae0f560ac23fef1365871e1b7
zarif ahmed <zarifahmed14@gmail.com>
2026-02-15 21:54:12 +0600

    Add Clerk Next.js app scaffold and auth-related app updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Inter} from "next/font/google";
 
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider,
+          SignedOut,
+          SignInButton,
+          SignUpButton,
+          UserButton, 
+} from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const plexMono = IBM_Plex_Mono({
+  variable: "--font-plex-mono",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -26,18 +35,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    appearance={{
+      theme: dark,
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
-            disableTransitionOnChange>
+            disableTransitionOnChange
+            >
+              <header>
+              <SignedOut>
+                <SignInButton/>
+                <SignUpButton>
+                  <button className="bg-rose-500 text-white p-2 rounded"/>
+                </SignUpButton>
+              </SignedOut>
+              </header>
           {children}
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
